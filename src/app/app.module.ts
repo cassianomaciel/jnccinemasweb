20a2e8877eb13b1bf7ebeac09e8db9b2cdc36a07
Cassiano <cmduarte@restinga.ifrs.edu.br>
2021-07-16 11:49:06 -0300

    adição do angular material e remoção da app page padrão angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilmeformComponent } from './filmeform/filmeform.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmeformComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/filmeform/filmeform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filmeform',
+  templateUrl: './filmeform.component.html',
+  styleUrls: ['./filmeform.component.less']
+})
+export class FilmeformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
