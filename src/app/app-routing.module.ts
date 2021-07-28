4bf7a5c82b50dff6c754b4bd2f92680cc06c906f
Cassano <ninocb@gmail.com>
2021-07-27 22:59:05 -0300

    Commit 404 error e icone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './error404/error-404.component';
 import { HomeComponent } from './views/home/home.component';
 
 const routes: Routes = [
   {
     path:'',
     component: HomeComponent
+  },
+  {
+    path: '**', component: Error404Component
   }
-
 ];
 
 @NgModule({
